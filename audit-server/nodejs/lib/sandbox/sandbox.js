81f3e7fb0fd160b8caca0ed680adb7fa3ad12a2b
Kris Geusebroek <krisgeus@gmail.com>
2012-01-06 17:20:35 +0100

    testing sandbox

--- audit-server/nodejs/lib/sandbox/sandbox.js
+++ audit-server/nodejs/lib/sandbox/sandbox.js
@@ -1,7 +1,9 @@
 var util = require('util')
 var events = require('events')
+var fs = require('fs')
+var childproc = require('child_process')
 
-module.exports.createSandbox = function(uuid, user, gitRepo, gitTree, callback) {
+module.exports.createSandbox = function(uuid, user, gitRepo, gitTree) {
 	return new Sandbox(uuid, user, gitRepo, gitTree)
 }
 
@@ -14,19 +16,48 @@ function Sandbox(uuid, user, gitRepo, gitTree) {
 	var self = this
 	
 	this.build = function(callback) {
-		self.on('error', function(err) {
-			callback(err)
+		
+		var message
+		var sandboxDir = auditserver.config.sandboxdir+ '/' + self.user + '-' + self.uuid
+		self.emit('output', 'creating sandbox directory')
+		var userid, groupid = 0
+		
+		childproc.execFile('/usr/bin/id', ['-u', self.user], { }, function(error, stdout, stderr) {
+			if (error) {
+				message = stderr
+				self.emit('end', error, message)
+				callback(error, message)
+			} else {
+				userid = parseInt(stdout)
+				childproc.execFile('/usr/bin/id', ['-g', self.user], { }, function(error, stdout, stderr) {
+					if (error) {
+						message = stderr
+						self.emit('end', error, message)
+						callback(error, message)
+					} else {
+						groupid = parseInt(stdout)
+						fs.mkdir(sandboxDir, '0660', function(err) {
+							if (err) {
+								message = 'Error while creating sandbox directory'
+								self.emit('end', err, message)
+								callback(err, message)
+							} else {
+								fs.chown(sandboxDir, userid, groupid, function(err) {
+									if (err) {
+										message = 'Error while change ownership of sandbox directory'
+										self.emit('end', err, message)
+										callback(err, message)
+									} else {
+										self.emit('end', null, uuid)
+										callback(null, uuid)
+									}
+								})
+							}
+						})
+					}
+				})
+			}
 		})
-		self.on('end', function(data) {
-			callback(null, data)
-		})
-		
-		makeDirectory()
-		
-		function makeDirectory(callback) {
-			self.emit('end', 'Not implemented yet', 'NOK')
-		}
-		
 	}
 	
 	this.remove = function() {
--- audit-server/nodejs/tests/sandbox/test-sandbox.js
+++ audit-server/nodejs/tests/sandbox/test-sandbox.js
@@ -1,16 +1,115 @@
-var recipe = require('../../lib/sandbox/sandbox')
+var sandbox = require('../../lib/sandbox/sandbox')
+var fs = require('fs')
+var removeRecursively = require('rimraf')
+var childproc = require('child_process')
+
+var box, user, token, tree
+
+function getGitCommitSha(gitDir, callback) {
+	var theCommit = ''
+	var show = childproc.execFile(
+		'/usr/bin/git show | grep commit', 
+		[],
+		{ "cwd" : gitDir }
+	)
+	show.on('data', function(d) { theCommit += d })
+	show.on('exit', function() { console.log('in exit, the commit = '+theCommit); callback(theCommit) })
+}
 
 exports.setUp = function(callback) {
+	user = 'test-user'
+	token = 'UUID1234-TEST-UUID-1234-THISISATESTUUID123'
+	tree = '8e40a45c79ebf9ca36685e2c228254b87f3d67af'
+	global.auditserver = {
+		config : {
+			basedir : __dirname,
+			sandboxdir : __dirname + '/test-sandbox-dir'
+		}
+	}
+	
+	box = sandbox.createSandbox(token, user, __dirname + '/local-git-repo/test-repo.git/', tree)
 	callback()
 }
 
 exports.tearDown = function(callback) {
-	callback()
+	fs.stat(auditserver.config.sandboxdir+ '/' + user + '-' + token, function(statErr) {
+		if (!statErr) {
+			removeRecursively(global.auditserver.config.sandboxdir + '/' + user + '-' + token, {}, callback)
+		} else { 
+			callback() 
+		}
+	})
 }
 
-exports.templateTest = function(test){
+exports.successfullyCreateAndFillASandboxWithRepoAndTree = function(test){
+
+	box.build(runAsserts)
+	
+	function runAsserts(err, data) {
+	    test.expect(4)
+		test.strictEqual(err, null, 'No error needs to be present')
+		test.equal(data, token, 'Returned the correct token')
+		
+		fs.stat(auditserver.config.sandboxdir+ '/' + user + '-' + token, function(statErr) {
+			test.strictEqual(statErr, null, 'directory needs to be present!')
+
+			getGitCommitSha(auditserver.config.sandboxdir + '/' + user + '-' + token + '/checkout/', function(gitCommit) {
+				test.equal(gitCommit, tree, 'Returned the correct commit Sha')
+				test.done()
+			})
+		})
+
+	}
+}
+
+exports.successfullyCreateAndFillASandboxWithRepoAndNoTree = function(test){
+
+	box.tree = undefined
+	var expectedCommitSha = '43cb32934458101b1531d079f54b58aee1ef940b'
+	box.build(runAsserts)
+	
+	function runAsserts(err, data) {
+	    test.expect(4)
+		test.strictEqual(err, null, 'No error needs to be present')
+		test.equal(data, token, 'Returned the correct token')
+		
+		fs.stat(auditserver.config.sandboxdir+ '/' + user + '-' + token, function(statErr) {
+			test.strictEqual(statErr, null, 'directory needs to be present!')
+
+			getGitCommitSha(auditserver.config.sandboxdir + '/' + user + '-' + token + '/checkout/', function(gitCommit) {
+			test.equal(gitCommit, expectedCommitSha, 'Returned the correct commit Sha')
+				test.done()
+			})
+		})
+	}
+}
+
+exports.FailOnUnknownOSUserWhileCreatingASandbox = function(test){
+	box.user = 'unknown-user'
+	box.build(runAsserts)
+	
+	function runAsserts(err, data) {
+	    test.expect(3)
+		test.notStrictEqual(err, null, 'Error needs to be present!')
+		test.equal(data, 'id: unknown-user: no such user\n', 'Returned wrong error message')
+		
+		fs.stat(auditserver.config.sandboxdir+ '/' + user + '-' + token, function(statErr) {
+			test.notStrictEqual(statErr, null, 'Sandbox must not exist')
+			test.done()
+		})
+	}
+}
+
+
+exports.FailOnUnknownGitUrlWhileCreatingASandbox = function(test){
     test.expect(1)
-	test.ok(true, 'Some message that can be shown')
+	test.ok(false, 'FailOnUnknownGitUrlWhileCreatingASandbox not implemented yet')
+	test.done()
+}
+
+exports.FailOnUnknownTreeWhileCreatingASandbox = function(test){
+    test.expect(1)
+	test.ok(false, 'FailOnUnknownTreeWhileCreatingASandbox not implemented yet')
 	test.done()
 }
 
