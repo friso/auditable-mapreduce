248af241cc96f6b65e1b4ff84b248d823f7fca70
Kris Geusebroek <krisgeus@gmail.com>
2012-01-11 14:02:27 +0100

    made script use stop start restart params

--- audit-server/nodejs/auditserver.sh
+++ audit-server/nodejs/auditserver.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
-sudo -u auditserver /usr/local/bin/node audit-server.js $@  > /var/log/auditserver.log 2> /var/log/auditserver.err &
+function start() {
+    if [ -f /var/run/auditserver/auditserver.pid ]
+    then
+        echo "Auditserver already running. First stop or try the restart option."
+        exit 1
+    else
+        #sudo -u auditserver /usr/local/bin/node audit-server.js > /var/log/auditserver/auditserver.log 2> /var/log/auditserver/auditserver.err &
+        /usr/local/bin/node audit-server.js > /var/log/auditserver/auditserver.log 2> /var/log/auditserver/auditserver.err &
 
+        PID=$!
+        echo $PID > /var/run/auditserver/auditserver.pid
+        exit 0
+    fi
+}
 
+function stop() {
+    if [ -f /var/run/auditserver/auditserver.pid ]
+    then
+        PID=`cat /var/run/auditserver/auditserver.pid`
+        kill $PID
+	rm /var/run/auditserver/auditserver.pid
+    else
+        echo "No running auditserver found to stop"
+        exit 1
+    fi
+}
+
+case "$1" in
+        start)
+            start
+            ;;
+         
+        stop)
+            stop
+            ;;
+         
+        restart)
+            stop
+            start
+            ;;
+
+        *)
+            echo $"Usage: $0 {start|stop|restart}"
+            exit 1
+esac
